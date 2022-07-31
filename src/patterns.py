d85d1059117f7eda951aff45561fd319fe9bc3db
nhazoury <nhazoury14@gmail.com>
2022-07-31 12:31:10 +0100

    refactor: delete extra empty lines

--- src/patterns.py
+++ src/patterns.py
@@ -5,19 +5,15 @@ class PatternMaker:
     def __init__(self, c: CellHandler):
         self.c = c
 
-
-
     def blinker(self, x, y):
         self.c.set_live(x, y)
         self.c.set_live(x + 1, y)
         self.c.set_live(x + 2, y)
 
-
     def glider(self, x, y):
         self.blinker(x, y)
         self.c.set_live(x, y + 1)
         self.c.set_live(x + 1, y + 2)
-
 
     def f_pentomino(self, x, y):
         self.c.set_live(x + 1, y)
